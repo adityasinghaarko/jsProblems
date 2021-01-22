47a80ded9718ab10b806aebfbc6515a227561042
adityasingha <adityasingha659@gmail.com>
2021-01-22 14:00:30 +0600

    first problem completed

---
+++ assignment.js
@@ -0,0 +1,12 @@
+// https://github.com/adityasinghaarko/jsProblems
+
+// Solution of the first problem:
+
+function kilometerToMeter(km){
+    var result = km * 1000;       //1km = 1000m
+    return result;
+}
+var fourKm = kilometerToMeter(4);
+console.log(fourKm);
+
+
